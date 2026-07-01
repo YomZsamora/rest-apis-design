812643b2580cb66c7eee17dd256b0e12d8f5bb43
yomZsamora <samaurah@gmail.com>
2026-07-01 14:59:07 +0300

    feat: add custom exception classes for error handling

---
+++ src/utils/exceptions/custom-exceptions.js
@@ -0,0 +1,45 @@
+
+class BadRequest extends Error {
+
+    constructor(message, errors = null) {
+        super(message);
+        this.name = "BadRequest";
+        this.errors = errors;
+        this.statusCode = 400;
+    }
+}
+
+class NotFound extends Error {
+
+    constructor(message) {
+        super(message);
+        this.name = "NotFound";
+        this.statusCode = 404;
+    }
+}
+
+class NotAuthenticated extends Error {
+
+    constructor() {
+        super("Authentication credentials were not provided.");
+        this.statusCode = 401
+        this.name = "NotAuthenticated";
+    }
+}
+
+class PermissionDenied extends Error {
+
+    constructor() {
+        super("You don't have required permission to perform this action.");
+        this.statusCode = 403
+        this.name = "PermissionDenied";
+    }
+}
+
+
+module.exports = { 
+    BadRequest, 
+    NotFound, 
+    NotAuthenticated, 
+    PermissionDenied
+};
