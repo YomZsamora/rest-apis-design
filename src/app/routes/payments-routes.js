c07a5fb097ad62a4beb64d74c4dbc2d1726ff92c
yomZsamora <samaurah@gmail.com>
2026-07-01 15:33:23 +0300

    feat: remove payments routes and associated handlers

--- src/app/routes/payments-routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { idempotencyMiddleware } = require('../middlewares/idempotency-middleware');
-const { idempotentPaymentHandler } = require('../handlers/idempotent-payment-handler');
-
-const router = express.Router();
-
-router.post('/', idempotencyMiddleware, idempotentPaymentHandler);
-
-module.exports = router;
