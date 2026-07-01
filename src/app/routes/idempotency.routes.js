2a00d87a4f39aa49f72be31adc9f4e9502408fc4
yomZsamora <samaurah@gmail.com>
2026-07-01 17:17:32 +0300

    refactor: remove idempotency middleware and related controller and routes

--- src/app/routes/idempotency.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { idempotencyMiddleware } = require('../middlewares/idempotency.middleware');
-const { idempotentPaymentHandler } = require('../controllers/idempotent-charge.controller');
-
-const router = express.Router();
-
-router.post('/charge', idempotencyMiddleware, idempotentPaymentHandler);
-
-module.exports = router;
