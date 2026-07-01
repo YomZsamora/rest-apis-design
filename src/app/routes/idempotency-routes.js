9e4d812e809af587d747483a07e250da2d3218be
yomZsamora <samaurah@gmail.com>
2026-07-01 18:52:34 +0300

    refactor: rename router variable to v1Router for clarity

--- src/app/routes/idempotency-routes.js
+++ src/app/routes/idempotency-routes.js
@@ -2,8 +2,8 @@ const express = require('express');
 const { IdempotencyMiddlewares } = require('../middlewares/idempotency-middleware');
 const { idempotentPaymentHandler } = require('../controllers/idempotent-charge-controller');
 
-const router = express.Router();
+const v1Router = express.Router();
 
-router.post('/charge', IdempotencyMiddlewares, idempotentPaymentHandler);
+v1Router.post('/charge', IdempotencyMiddlewares, idempotentPaymentHandler);
 
-module.exports = router;
+module.exports = v1Router;
